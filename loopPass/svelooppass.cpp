8a2bc9e94e3e40fd12cac4199c467569cb3f1ac7
Quinn Pham <quinnlp12@gmail.com>
2020-06-08 14:13:11 -0600

    Naming convention fix

--- loopPass/svelooppass.cpp
+++ loopPass/svelooppass.cpp
@@ -6,42 +6,59 @@
 #include "llvm/IR/DebugLoc.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/InitializePasses.h"
+#include "llvm/ADT/APFloat.h"
 
 using namespace llvm;
 
-#define DEBUG_TYPE "svelooppass"
+#define DEBUG_TYPE "SVELoopPass"
+
+namespace llvm {
+void initializeSVELoopPassPass(llvm::PassRegistry&);
+}
 
 namespace {
 
-  class MyFirstLoopPass: public LoopPass {
+  class SVELoopPass: public LoopPass {
   public:
     static char ID;
-    MyFirstLoopPass() : LoopPass(ID) {}
+    SVELoopPass() : LoopPass(ID) {}
     bool runOnLoop(Loop *L, LPPassManager &LPM) override;
+    void getAnalysisUsage(AnalysisUsage &AU) const override {
+      AU.addRequired<DominatorTreeWrapperPass>();
+    }
+
   };
 
-  bool MyFirstLoopPass::runOnLoop(Loop *L, LPPassManager &LPM) {
+  bool SVELoopPass::runOnLoop(Loop *L, LPPassManager &LPM) {
 
-    //errs() << "loop at line #" << L->getLocRange().getStart()->getLine() << '\n';
-    if (L->getNumBlocks() > 0) {        // if the loop has control flow
-      bool is_innermost = true;
-      for (Loop *O : L->getLoopsInPreorder()) {
-        //errs() << "innerLoop at line #" << O->getLocRange().getStart()->getLine() << '\n';
-        if (O != L) {                   // contains another loop
-          is_innermost = false;  
-          break;    
-        }
+    bool is_innermost = true;
+    for (Loop *loop : L->getLoopsInPreorder()) {
+      if (loop != L) {                   // contains another loop
+        is_innermost = false;
+        break;
       }
-      if (is_innermost) {                      // is an innermost loop
-        errs()  << "Line #" << L->getLocRange().getStart()->getLine() 
-        << "\t# Blocks: " << L->getNumBlocks() <<'\n';
+    }
+    if (is_innermost) {                  // is an innermost loop
+      for (BasicBlock *block : L->getBlocks()) {
+        DominatorTree &DT = LPM.getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+        //SmallVector<BasicBlock *, 100> Result;
+        //DT.getDescendants(block, Result);
+        //for (BasicBlock *block : L->getBlocks()) {}
       }
+      errs()  << "Line #" << L->getLocRange().getStart()->getLine() << "\t# Blocks: " << L->getNumBlocks() <<'\n';
     }
     return false;
   }
 
+
 } // end of anonymous namespace
 
-char MyFirstLoopPass::ID = 0;
-static RegisterPass<MyFirstLoopPass> X("svelooppass",
-  "svelooppass: pass that prints the starting line of each loop", true, true);
+
+
+char SVELoopPass::ID = 0;
+INITIALIZE_PASS_BEGIN(SVELoopPass, "SVELoopPass", "SVELoopPass", false, false)
+INITIALIZE_PASS_DEPENDENCY(DominatorTreeWrapperPass)
+INITIALIZE_PASS_END(SVELoopPass, "SVELoopPass", "SVELoopPass", false, false)
+static RegisterPass<SVELoopPass> X("SVELoopPass", "SVELoopPass: pass that prints the starting line of each loop", true, true);
